d7556bc4780cbb7c4933711a4b4f4a581b620611
Ibrahim Aslan <halaslan@stu.okan.edu.tr>
2021-01-19 02:38:50 +0300

    ODEV3

---
+++ Odev3/DOSYALAR/app/src/main/java/user/rgst/MainActivity.kt
@@ -0,0 +1,28 @@
+package user.rgst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
+import user.rgst.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val viewModel = RegisterViewModel(this)
+        binding.viewModel = viewModel
+    }
+
+    fun toastMessage(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    fun registerSuccess() {
+        binding.root.visibility = ConstraintLayout.INVISIBLE
+        toastMessage("Kayıt başarılı.")
+    }
+}
